8c5df9d445c61228eaf1a006c5a4e97d0449ac50
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2023-08-06 23:47:40 +0100

    Fixed responsive issues for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,95 +1,143 @@
 import React from "react";
 import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
 
+const hamburgerIcon = (
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="24" height="24" rx="4" fill="transparent" fillOpacity="0" />
+    <rect
+      x="4"
+      y="12"
+      width="16"
+      height="2"
+      rx="1"
+      fill="#FFFFFF" // White color for the bars
+    />
+    <rect
+      x="4"
+      y="6"
+      width="16"
+      height="2"
+      rx="1"
+      fill="#FFFFFF" // White color for the bars
+    />
+    <rect
+      x="4"
+      y="18"
+      width="16"
+      height="2"
+      rx="1"
+      fill="#FFFFFF" // White color for the bars
+    />
+  </svg>
+);
+
 const NavbarComponent = () => {
   return (
-    <Navbar className="gold-text-navbar" expand="sm">
-      <Container>
-        <Navbar.Brand href="/">
-          <img
-            src="../../images/pacific_logo.png"
-            className="d-inline-block align-top mb-5 mt-3"
-            alt="Gig Mania Logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-between">
-          <Nav className="ms-auto">
-            <Nav.Link style={{ fontSize: "22px", color: "#eccf42" }} href="/">
-              Home
-            </Nav.Link>
+    <>
+      <style>
+        {`
+          /* Custom CSS for medium screens and below (<= 991px) */
+          @media (max-width: 991px) {
+            .gold-text-navbar .navbar-nav {
+              display: none; /* Hide the nav links */
+            }
 
-            <Dropdown as={Nav.Item}>
-              <Dropdown.Toggle
-                as={Nav.Link}
+            .gold-text-navbar .navbar-collapse.collapse.show .navbar-nav {
+              display: flex; /* Show the nav links when the hamburger menu is open */
+            }
+
+            /* Change the color of the hamburger icon to transparent */
+            .gold-text-navbar .navbar-toggler {
+              background-color: transparent;
+              border-color: transparent;
+            }
+          }
+        `}
+      </style>
+      <Navbar className="gold-text-navbar" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src="../../images/pacific_logo.png"
+              className="d-inline-block align-top mb-5 mt-3"
+              alt="Gig Mania Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav">
+            {hamburgerIcon}
+          </Navbar.Toggle>
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link style={{ fontSize: "22px", color: "#eccf42" }} href="/">
+                Home
+              </Nav.Link>
+
+              <Dropdown as={Nav.Item}>
+                <Dropdown.Toggle
+                  as={Nav.Link}
+                  style={{ fontSize: "22px", color: "#eccf42" }}
+                >
+                  The Gym
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/strengthroom">
+                    Strength Room
+                  </Dropdown.Item>
+                  <Dropdown.Item href="/legroom">Leg Room</Dropdown.Item>
+                  <Dropdown.Item href="/cardioandabsroom">
+                    Cardio and Abs Room
+                  </Dropdown.Item>
+                  <Dropdown.Item href="/chestandbackroom">
+                    Chest and Back Room
+                  </Dropdown.Item>
+                  <Dropdown.Item href="/shouldersandarmsroom">
+                    Shoulders and Arms Room
+                  </Dropdown.Item>
+                  <Dropdown.Item href="/hiitroom">HIIT Room</Dropdown.Item>
+                  <Dropdown.Item href="/boxingandbikesroom">
+                    Boxing And Bikes Room
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Nav.Link
                 style={{ fontSize: "22px", color: "#eccf42" }}
+                href="/theteam"
               >
-                The Gym
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item href="/strengthroom">
-                  Strength Room
-                </Dropdown.Item>
-                <Dropdown.Item href="/legroom">Leg Room</Dropdown.Item>
-                <Dropdown.Item href="/cardioandabsroom">
-                  Cardio and Abs Room
-                </Dropdown.Item>
-                <Dropdown.Item href="/chestandbackroom">
-                  Chest and Back Room
-                </Dropdown.Item>
-                <Dropdown.Item href="/shouldersandarmsroom">
-                  Shoulders and Arms Room
-                </Dropdown.Item>
-                <Dropdown.Item href="/hiitroom">HIIT Room</Dropdown.Item>
-                <Dropdown.Item href="/boxingandbikesroom">
-                  Boxing And Bikes Room
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+                The Team
+              </Nav.Link>
 
-            <Nav.Link
-              style={{ fontSize: "22px", color: "#eccf42" }}
-              href="/theteam"
-            >
-              The Team
-            </Nav.Link>
+              <Nav.Link
+                style={{ fontSize: "22px", color: "#eccf42" }}
+                href="/newsandevents"
+              >
+                News
+              </Nav.Link>
 
-            <Nav.Link
-              style={{ fontSize: "22px", color: "#eccf42" }}
-              href="/newsandevents"
-            >
-              News
-            </Nav.Link>
+              <Nav.Link
+                style={{ fontSize: "22px", color: "#eccf42" }}
+                href="/contact"
+              >
+                Contact
+              </Nav.Link>
 
-            <Nav.Link
-              style={{ fontSize: "22px", color: "#eccf42" }}
-              href="/contact"
-            >
-              Contact
-            </Nav.Link>
-
-            <Nav.Link
-              style={{ fontSize: "22px", color: "#eccf42" }}
-              href="/membership"
-            >
-              Membership
-            </Nav.Link>
-
-            <style>
-              {`
-                .nav-link:hover,
-                .active-link,
-                .dropdown-toggle:hover,
-                .dropdown-toggle:active,
-                .dropdown-menu.show {
-                  color: white !important;
-                }
-              `}
-            </style>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <Nav.Link
+                style={{ fontSize: "22px", color: "#eccf42" }}
+                href="/membership"
+              >
+                Membership
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
--- src/pages/TheTeam.js
+++ src/pages/TheTeam.js
@@ -10,13 +10,38 @@ import {
 export default function App() {
   return (
     <div>
+      <style>
+        {`
+          /* Custom CSS for medium screens and below (<= 991px) */
+          @media (max-width: 991px) {
+            .team-card {
+              margin-bottom: 30px; /* Add some margin between the cards */
+            }
+          }
+
+          /* Custom CSS for large screens (>= 992px) */
+          @media (min-width: 992px) {
+            /* Center the entire team section */
+            .team-section {
+              display: flex;
+              justify-content: center;
+            }
+
+            /* Center the cards within the team section */
+            .team-card {
+              margin: 0 15px; /* Add some horizontal space between the cards */
+              height: 100%; /* Set a fixed height for the cards */
+            }
+          }
+        `}
+      </style>
       <h1 className="text-center pb-4" style={{ color: "#eccf42" }}>
         The Team
       </h1>
       <div className="container">
-        <div className="row">
+        <div className="row team-section">
           <div className="col-md-4">
-            <MDBCard>
+            <MDBCard className="team-card">
               <MDBCardImage
                 src="../../images/team-members/ali.png"
                 position="top"
@@ -40,7 +65,7 @@ export default function App() {
           </div>
 
           <div className="col-md-4">
-            <MDBCard>
+            <MDBCard className="team-card">
               <MDBCardImage
                 src="../../images/team-members/cath.png"
                 position="top"
@@ -64,7 +89,7 @@ export default function App() {
           </div>
 
           <div className="col-md-4">
-            <MDBCard>
+            <MDBCard className="team-card">
               <MDBCardImage
                 src="../../images/team-members/geraint.jpeg"
                 position="top"
@@ -78,67 +103,64 @@ export default function App() {
                   Geraint
                 </MDBCardTitle>
                 <MDBCardText className="text-center text-light">
-                  A dedicated powerlifter, Geraint loves all things strength.
-                  Beyond the gym, he enjoys outdoor adventures, often camping in
-                  remote areas like a Welsh Bear Grylls!
+                  A keen powerlifter, Geraint loves all things strength training
+                  and has even put on his own powerlifting competition. Beyond
+                  the gym, he enjoys outdoor adventures and camping in remote
+                  areas like a Welsh Bear Grylls.
                 </MDBCardText>
               </MDBCardBody>
             </MDBCard>
           </div>
 
-          <div className="col-md-4"></div>
-
-          <div className="d-flex justify-content-center align-items-center">
-            <div className="col-md-4 pt-3">
-              {" "}
-              <MDBCard>
-                <MDBCardImage
-                  src="../../images/team-members/liam.png"
-                  position="top"
-                  alt="..."
-                />
-                <MDBCardBody>
-                  <MDBCardTitle
-                    className="text-center"
-                    style={{ color: "#eccf42" }}
-                  >
-                    Liam
-                  </MDBCardTitle>
-                  <MDBCardText className="text-center text-light">
-                    Liam, with a masters degree in Sport and Exercise Science
-                    and a background in boxing and rugby, plays a pivotal role
-                    instructing the gym's Circuits, Bootcamp, and Boxing
-                    circuits classes.
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </div>
-
-            <div className="col-md-4 pt-3" style={{ margin: "25px" }}>
-              {" "}
-              <MDBCard>
-                <MDBCardImage
-                  src="../../images/team-members/peachy.png"
-                  position="top"
-                  alt="..."
-                />
-                <MDBCardBody>
-                  <MDBCardTitle
-                    className="text-center"
-                    style={{ color: "#eccf42" }}
-                  >
-                    Richard
-                  </MDBCardTitle>
-                  <MDBCardText className="text-center text-light">
-                    Richard, a former Welsh Champion Powerlifter with an
-                    impressive range of talents, is a dab hand at almost
-                    everything. His versatility makes him a valuable asset,
-                    contributing to various aspects of the gym.
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </div>
+          {/* Start of the new row for Liam and Richard */}
+          <div className="col-md-4 pt-4 team-card">
+            <MDBCard className="team-card">
+              <MDBCardImage
+                src="../../images/team-members/liam.png"
+                position="top"
+                alt="..."
+              />
+              <MDBCardBody>
+                <MDBCardTitle
+                  className="text-center"
+                  style={{ color: "#eccf42" }}
+                >
+                  Liam
+                </MDBCardTitle>
+                <MDBCardText className="text-center text-light">
+                  Liam, with a masters degree in Sport and Exercise Science and
+                  a background in boxing and rugby, plays a pivotal role
+                  instructing the gym's Circuits, Bootcamp, and Boxing circuits
+                  classes.
+                </MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
           </div>
+
+          <div className="col-md-4 pt-4 team-card">
+            <MDBCard className="team-card">
+              <MDBCardImage
+                src="../../images/team-members/peachy.png"
+                position="top"
+                alt="..."
+              />
+              <MDBCardBody>
+                <MDBCardTitle
+                  className="text-center"
+                  style={{ color: "#eccf42" }}
+                >
+                  Richard
+                </MDBCardTitle>
+                <MDBCardText className="text-center text-light">
+                  Richard, a former Welsh Champion Powerlifter with a range of
+                  talents, is a dab hand at almost everything. His versatility
+                  makes him a valuable asset, contributing to various aspects of
+                  the gym.
+                </MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+          {/* End of the new row for Liam and Richard */}
         </div>
       </div>
     </div>
